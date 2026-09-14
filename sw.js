2498922e0e8b5370662524819ec35ae34d63d486
freecoffee-sync <sync@freecoffee.tech>
2026-09-15 04:01:12 +0530

    Sync public site from dev repo (bffd0b5)

--- sw.js
+++ sw.js
@@ -22,8 +22,8 @@
 /* eslint-disable no-restricted-globals */
 'use strict';
 
-var ASSET_V = '117';                  // must match ?v= in the HTML asset URLs
-var CACHE = 'freecoffee-v117';
+var ASSET_V = '118';                  // must match ?v= in the HTML asset URLs
+var CACHE = 'freecoffee-v118';
 
 var PRECACHE = [
   './',
