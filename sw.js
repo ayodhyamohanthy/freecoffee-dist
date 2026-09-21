cc195016d269e7b81d7d36c727d39bd9ee6bd723
AyodhyaRam Mohanthy <mayodhya.14.cstmtech@anits.edu.in>
2026-09-21 09:59:12 -0400

    Bump asset version to 1002009 for dashboard grid fix

--- sw.js
+++ sw.js
@@ -25,8 +25,8 @@
 /* eslint-disable no-restricted-globals */
 'use strict';
 
-var ASSET_V = '1002008';                  // must match ?v= in the HTML asset URLs
-var CACHE = 'freecoffee-v1002008';
+var ASSET_V = '1002009';                  // must match ?v= in the HTML asset URLs
+var CACHE = 'freecoffee-v1002009';
 
 var PRECACHE = [
   './',
